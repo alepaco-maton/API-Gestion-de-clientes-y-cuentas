817fb41eb509874f1df4617d6744db0ca9588db6
alepaco.maton <alepaco.maton@gmail.com>
2024-09-14 22:07:48 -0400

    Actualizando Notacion JPA

--- src/main/java/bo/com/bancounion/entity/CustomerAccount.java
+++ src/main/java/bo/com/bancounion/entity/CustomerAccount.java
@@ -12,7 +12,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import java.math.BigDecimal;
